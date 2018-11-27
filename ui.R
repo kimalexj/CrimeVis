011532ae170aa5d3d3d5f208176e144b75e37ad4
Alexander Kim <alaki611@gmail.com>
2018-11-27 14:12:11 -0800

    Making modifications to repo and ui

--- ui.R
+++ ui.R
@@ -1,7 +1,40 @@
 library(shiny)
+library(leaflet)
+library(shinythemes)
 
-shinyUI(fluidPage(
-  
-  titlePanel("")
-  
+shinyUI(fluidPage(theme = shinytheme("darkly"),
+  navbarPage("CrimeVis",
+             
+    tabPanel("Home",
+      sidebarPanel(
+        # Validation necessary 
+        textInput("neighborhoodEntry", label = h3("Search Neighborhood"), value = "Enter neighborhood here... "),
+        dateRangeInput("dateRange", label = h3("Date range")),
+        selectInput("select", label = h3("Time"), choices = list("AM" = "Morning", "PM" = "Night", "Both" = "MornNight"), 
+                    selected = "MornNight"),
+        checkboxGroupInput("filters", label = h3("Type Filter"), 
+                           choices = list("Theft" = "THEFT", "Burglary" = "BURGLARY", "Narcotics" = "NARC",
+                           "Tresspassing" = "TRESSPASS", "Vehicle" = "VEH", "Children Affiliated" = "CHILD", 
+                           "Arson" = "ARSON", "Assault" = "ASSLT", "Rape" = "RAPE", "Sexual Offense" = "SEXOFF",
+                           "Homicide" = "HOMICIDE", "Prostitution" = "PROSTITUTION", "Obscene Material" = "PORNOGRAPHY",
+                           "DUI" = "DUI"))
+      ),
+      mainPanel(
+        
+      )
+    ),
+    
+    tabPanel("Crime Type"
+    
+    ),
+    
+    tabPanel("Crime Trend"
+    
+    ),
+    
+    tabPanel("Resources"
+    
+    )
+    
+  )
 ))
