0e93be1e271a4ff9c8f6aa5c84c96029ad3a092d
Alexander Kim <alaki611@gmail.com>
2018-12-01 15:28:12 -0800

    Finished Home Page

--- ui.R
+++ ui.R
@@ -8,15 +8,19 @@ shinyUI(fluidPage(theme = shinytheme("darkly"),
     tabPanel("Home",
       sidebarPanel(
         # Validation "not necessarily for error" but to see if any crime was in the area 
-        textInput("neighborhoodEntry", label = h3("Search Neighborhood"), value = "Enter neighborhood here... "),
-        selectInput("select", label = h3("Time of Day"), choices = list("AM" = "Morning", "PM" = "Night", "Both" = "MornNight"), 
-                    selected = "MornNight"),
+        textInput("neighborhoodEntry", label = h4("Search Neighborhood"), placeholder = "Enter neighborhood i.e. SANDPOINT", 
+                  value = "SANDPOINT"),
+        selectInput("select", label = h4("Time of Day"), choices = list("AM" = "AM", "PM" = "PM", "Both" = "0000"), 
+                    selected = "0000"),
+        selectInput("selectType", label = h4("Map Type"), choices = list("Current Crimes" = "current", "Neighborhoods" = "neighbor"),
+                    selected = "current"),
         column(6,
           br(),
-          checkboxGroupInput("filters1", label = h3("Type Filter"), 
+          checkboxGroupInput("filters1", label = h4("Type Filter"), 
                              choices = list("Theft" = "THEFT", "Burglary" = "BURGLARY", "Narcotics" = "NARC",
                              "Tresspassing" = "TRESSPASS", "Vehicle" = "VEH", "Children Affiliated" = "CHILD", 
-                             "Arson" = "ARSON", "Assault" = "ASSLT", "Rape" = "RAPE"))
+                             "Arson" = "ARSON", "Assault" = "ASSLT", "Rape" = "RAPE"), selected = c("THEFT", "BURGLARY",
+                             "VEH", "THEFT"))
         ),
         column(6,
           br(),
@@ -25,11 +29,15 @@ shinyUI(fluidPage(theme = shinytheme("darkly"),
           checkboxGroupInput("filters2", label = "", 
                              choices = list("Sexual Offense" = "SEXOFF",
                              "Homicide" = "HOMICIDE", "Prostitution" = "PROSTITUTION", "Obscene Material" = "PORNOGRAPHY",
-                             "DUI" = "DUI", "Robbery" = "ROBBERY", "Fraud" = "FRAUD", "Threats" = "THREATS"))       
+                             "DUI" = "DUI", "Robbery" = "ROBBERY", "Fraud" = "FRAUD", "Threats" = "THREATS", "Forgery" = "FORGERY"),
+                             selected = c("FRAUD", "THREATS", "FORGERY"))       
         )
       ),
       mainPanel(
-        leafletOutput("main_map", width = 900, height = 675)
+        tabsetPanel(type = "tabs",
+          tabPanel("Primary Map", leafletOutput("main_map", width = 900, height = 600)),
+          tabPanel("Neighborhood Plot", plotOutput("neighborhoodType", width = 900, height = 600))
+        )
       )
     ),
     
